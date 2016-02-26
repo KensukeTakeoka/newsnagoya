5b35d041da568a290d2c3673c4f1c9292feea3c3
KensukeTakeoka <t.kensk8@gmail.com>
2016-02-26 11:58:44 +0800

    地図ピンつけました。

---
+++ newsnagoya/recommendViewController.swift
@@ -0,0 +1,120 @@
+//
+//  recommendViewController.swift
+//  newsnagoya
+//
+//  Created by 武岡健介 on 2016/02/26.
+//  Copyright © 2016年 Takeoka Kensuke. All rights reserved.
+//
+import MapKit
+import UIKit
+
+class recommendViewController: UIViewController {
+
+    @IBOutlet weak var recommendMapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let coordinate = CLLocationCoordinate2DMake(35.170915,136.881537 )
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(coordinate, span)
+        recommendMapView.setRegion(region, animated:true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "title"
+        annotation.subtitle = "subtitle"
+        self.recommendMapView.addAnnotation(annotation)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - MKMapView delegate
+    
+    // Called when the region displayed by the map view is about to change
+    func mapView(mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        print(__FUNCTION__)
+    }
+    
+    // Called when the annotation was added
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView?.animatesDrop = true
+            pinView?.canShowCallout = true
+            pinView?.draggable = true
+            pinView?.pinColor = .Purple
+            
+            let rightButton: AnyObject! = UIButton(type: UIButtonType.DetailDisclosure)
+            pinView?.rightCalloutAccessoryView = rightButton as? UIView
+        }
+        else {
+            pinView?.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        print(__FUNCTION__)
+        if control == view.rightCalloutAccessoryView {
+            performSegueWithIdentifier("toTheMoon", sender: self)
+        }
+    }
+    
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, didChangeDragState newState: MKAnnotationViewDragState, fromOldState oldState: MKAnnotationViewDragState) {
+        if newState == MKAnnotationViewDragState.Ending {
+            let droppedAt = view.annotation?.coordinate
+            print(droppedAt)
+        }
+    }
+    
+    // MARK: - Navigation
+    
+    @IBAction func didReturnToMapViewController(segue: UIStoryboardSegue) {
+        print(__FUNCTION__)
+    }
+}
+
+
+        
+        
+        
+        
+        
+       
+//        let coodinate = CLLocationCoordinate2DMake(35.225748, 36.884252)
+//        
+//        
+//        //        縮尺を設定
+//        let span = MKCoordinateSpanMake(0.05,0.05)
+//        
+//        //        範囲オブジェクトを作成
+//        let region = MKCoordinateRegionMake(coodinate,span)
+//        
+//        //        mapViewに設定
+//        recommendMapView.setRegion(region, animated: true)
+        
+        
+        
+
+
+        
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
